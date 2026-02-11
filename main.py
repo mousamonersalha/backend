bb422e6ca096d8dc79f83bf02ba2dac7bf6990b7
mousamonersalha <mousasalha.201@gmail.com>
2026-02-11 19:50:08 +0200

    Updated tumor percentage calculation

--- main.py
+++ main.py
@@ -150,3 +150,17 @@ async def full_pipeline(file: UploadFile = File(...)):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port
+    )
